09d385b9a1ec77f198464dc439bd65ecbe3e5f32
Lejonbjorn <dante.lejonbjorn@elev.ga.ntig.se>
2024-02-26 12:01:18 +0100

    making the first page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-// Denna fil skall innehålla JavaScript (JS)
+const hamburger = document.querySelector("#hamburger");
+const nav = document.querySelector("nav")
+
+hamburger.addEventListener('dbclick',toggleMenu);
+
+function toggleMenu() {
+    nav.classList.toggle("active");
+}
